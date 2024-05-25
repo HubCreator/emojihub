75e86a9583efcedf7502a8e743ef0fe8d5867f5b
hyunseung-jung <hyunseung-jung@nhn-commerce.com>
2024-05-25 14:18:08 +0900

    initial commit

---
+++ src/main/kotlin/com/github/hubcreator/emojihub/EmojihubApplication.kt
@@ -0,0 +1,11 @@
+package com.github.hubcreator.emojihub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmojihubApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmojihubApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/hubcreator/emojihub/EmojihubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.hubcreator.emojihub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmojihubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
